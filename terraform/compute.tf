ed252b2e979f3f1b3680cc06eca42f811803bb08
Li Weinan <li.weinan@rea-group.com>
2020-09-30 14:09:55 +0800

    persist instance root volume

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -39,6 +39,9 @@ resource "aws_instance" "jenkins-instance" {
   user_data = "${file("install_jenkins.sh")}"
 
   associate_public_ip_address = true
+  root_block_device {
+    delete_on_termination = false
+  }
   tags = {
     Name = "Jenkins-Instance"
   }
@@ -48,7 +51,6 @@ resource "aws_security_group" "sg_allow_ssh_jenkins" {
   name        = "allow_ssh_jenkins"
   description = "Allow SSH and Jenkins inbound traffic"
   vpc_id      = "${aws_vpc.development-vpc.id}"
-
   ingress {
     from_port   = 22
     to_port     = 22
